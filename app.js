6211226370438902fd913b7e2137bd303333e821
Uchannel92 <uchenna_attah@homtail.com>
2021-07-01 12:31:42 +0100

    Added new functions to delete items from DOM. as well as applying dataset index on rows

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const veg_form = document.querySelector('#form')
 const basket = []
 
-function Plants() {
-
-}
-
 function Veg(type, color, taste) {
     this.type = type
     this.color = color
     this.taste = taste
 }
 
-Veg.prototype = Object.create(Plants.prototype)
-
 let vegetable = new Veg('spring onion', 'white', true)
 
-function createVegObject(e) {
+function addVegToBasket(e) {
     e.preventDefault()
     let new_veg = new Veg(e.target.elements.veg.value, e.target.elements.color.value, confirmTaste())
     
@@ -36,6 +30,8 @@ function clearForm(e) {
     e.preventDefault()
     e.target.elements.veg.value = ''
     e.target.elements.color.value = ''
+    document.querySelectorAll('input')[2].checked = false
+    document.querySelectorAll('input')[3].checked = false
 }
 
 function displayBasket() {
@@ -43,7 +39,7 @@ function displayBasket() {
     let tbody = document.querySelector('tbody')
     for (let i = 0; i < basket.length; i++) {
         html += `
-        <tr data-type="${i}">
+        <tr>
         <td>${basket[i].type}</td>
         <td>${basket[i].color}</td>
         <td>${basket[i].taste}</td>
@@ -53,7 +49,37 @@ function displayBasket() {
     }
     tbody.innerHTML = html   
 }
+
+// associate DOM elements with basket array
+function applyDataSet() {
+    let rows = document.querySelectorAll('tr')
+    for (let i = 0; i < rows.length; i++){
+        rows[i].dataset.index = i
+        console.log(rows[i].dataset)
+    }
+}
+
+function deleteRow() {
+    let buttons = document.querySelectorAll('button')
+
+    // start from the second button as the first button adds the new Rows!
+    for (let i = 1; i < buttons.length; i++){
+        buttons[i].addEventListener('click', deleteButton)
+    }
+    function deleteButton() {
+        let row = document.querySelectorAll('tr')[0].remove()
+    }
     
-veg_form.addEventListener('submit', createVegObject)
+}
+
+function deleteFromArray() {
+    for (let i = 0; i < basket.length; i++) {
+        basket.splice(parseInt(document.querySelectorAll('tr')[0].dataset.index),1)
+        }
+}
+
+veg_form.addEventListener('submit', addVegToBasket)
 veg_form.addEventListener('submit', clearForm)
 veg_form.addEventListener('submit', displayBasket)
+veg_form.addEventListener('submit', applyDataSet)
+veg_form.addEventListener('submit', deleteRow)
